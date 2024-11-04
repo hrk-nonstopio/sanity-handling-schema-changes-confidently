68ee0f0872992052940dbbe653423467bd5440b3
Hrishikesh Kadam <hrishikesh.kadam@nonstopio.com>
2024-11-04 10:33:26 +0530

    Initial commit

---
+++ sanity-studio/sanity.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+import { structure } from './structure'
+import { defaultDocumentNode } from './structure/defaultDocumentNode'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Day one with Sanity',
+
+  projectId: 'dl4b6sy6',
+  dataset: 'production',
+
+  plugins: [
+    structureTool({ structure, defaultDocumentNode }),
+    visionTool()
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
